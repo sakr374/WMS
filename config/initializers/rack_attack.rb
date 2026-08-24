0bb5d88899aff8c0c4a4c27e762ea51a9ba8b01e
sakr374 <eiadahmed202205999@gmail.com>
2026-08-24 14:09:29 +0300

    Increase request throttle limit to 5000 per 5 minutes

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,6 @@
 class Rack::Attack
     # Throttle all requests by IP (e.g., 100 requests per minute)
-    throttle('req/ip', limit: 100, period: 1.minute) do |req|
+    throttle('req/ip', limit: 5000, period: 5.minute) do |req|
       req.ip
     end
   
